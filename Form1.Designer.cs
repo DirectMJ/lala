a4bf74711875a58f92d760b53a08f1a384888ec4
DirectMJ <138039347+DirectMJ@users.noreply.github.com>
2024-09-13 15:29:36 +0800

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace PattaguanMj
+﻿
+namespace _046_Pattaguan_Langcay_L3
 {
     partial class Form1
     {
@@ -28,222 +29,182 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.txtFn = new System.Windows.Forms.TextBox();
-            this.txtLn = new System.Windows.Forms.TextBox();
             this.cbDept = new System.Windows.Forms.ComboBox();
+            this.txtLn = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
             this.dtGridView = new System.Windows.Forms.DataGridView();
+            this.btnCancel = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
             this.btnReset = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.btnCancel = new System.Windows.Forms.Button();
-            this.btnAdd = new System.Windows.Forms.Button();
-            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.openFileD = new System.Windows.Forms.OpenFileDialog();
+            this.savetext = new System.Windows.Forms.MenuStrip();
             this.dataSourceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.openExcel = new System.Windows.Forms.ToolStripMenuItem();
+            this.excelFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.textFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.contextMenuStrip3 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.contextMenuStrip4 = new System.Windows.Forms.ContextMenuStrip(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dtGridView)).BeginInit();
-            this.menuStrip2.SuspendLayout();
+            this.savetext.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // contextMenuStrip1
-            // 
-            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
-            this.menuStrip1.Location = new System.Drawing.Point(0, 33);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1392, 24);
-            this.menuStrip1.TabIndex = 1;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // contextMenuStrip2
-            // 
-            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(24, 24);
-            this.contextMenuStrip2.Name = "contextMenuStrip2";
-            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
             // 
             // txtFn
             // 
-            this.txtFn.Location = new System.Drawing.Point(37, 152);
+            this.txtFn.Location = new System.Drawing.Point(28, 101);
             this.txtFn.Name = "txtFn";
-            this.txtFn.Size = new System.Drawing.Size(270, 26);
-            this.txtFn.TabIndex = 3;
-            // 
-            // txtLn
-            // 
-            this.txtLn.Location = new System.Drawing.Point(37, 237);
-            this.txtLn.Name = "txtLn";
-            this.txtLn.Size = new System.Drawing.Size(270, 26);
-            this.txtLn.TabIndex = 4;
+            this.txtFn.Size = new System.Drawing.Size(166, 20);
+            this.txtFn.TabIndex = 0;
             // 
             // cbDept
             // 
             this.cbDept.FormattingEnabled = true;
-            this.cbDept.Location = new System.Drawing.Point(37, 324);
+            this.cbDept.Location = new System.Drawing.Point(28, 219);
             this.cbDept.Name = "cbDept";
-            this.cbDept.Size = new System.Drawing.Size(270, 28);
-            this.cbDept.TabIndex = 5;
+            this.cbDept.Size = new System.Drawing.Size(166, 21);
+            this.cbDept.TabIndex = 1;
+            // 
+            // txtLn
+            // 
+            this.txtLn.Location = new System.Drawing.Point(28, 156);
+            this.txtLn.Name = "txtLn";
+            this.txtLn.Size = new System.Drawing.Size(166, 20);
+            this.txtLn.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(28, 269);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add to List";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // dtGridView
             // 
             this.dtGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtGridView.Location = new System.Drawing.Point(405, 27);
+            this.dtGridView.Location = new System.Drawing.Point(371, 36);
             this.dtGridView.Name = "dtGridView";
-            this.dtGridView.RowHeadersWidth = 62;
-            this.dtGridView.RowTemplate.Height = 28;
-            this.dtGridView.Size = new System.Drawing.Size(975, 591);
-            this.dtGridView.TabIndex = 6;
+            this.dtGridView.Size = new System.Drawing.Size(406, 303);
+            this.dtGridView.TabIndex = 4;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(119, 269);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(1078, 624);
+            this.btnSave.Location = new System.Drawing.Point(596, 367);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(111, 41);
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
             this.btnSave.TabIndex = 7;
-            this.btnSave.Text = "Save Record";
+            this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
             // 
             // btnReset
             // 
-            this.btnReset.Location = new System.Drawing.Point(1227, 624);
+            this.btnReset.Location = new System.Drawing.Point(677, 367);
             this.btnReset.Name = "btnReset";
-            this.btnReset.Size = new System.Drawing.Size(104, 41);
+            this.btnReset.Size = new System.Drawing.Size(100, 23);
             this.btnReset.TabIndex = 8;
-            this.btnReset.Text = "Reset Form";
+            this.btnReset.Text = "Reset Records";
             this.btnReset.UseVisualStyleBackColor = true;
             this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(33, 129);
+            this.label1.Location = new System.Drawing.Point(25, 85);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.Size = new System.Drawing.Size(57, 13);
             this.label1.TabIndex = 9;
             this.label1.Text = "First Name";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(33, 214);
+            this.label2.Location = new System.Drawing.Point(25, 140);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.Size = new System.Drawing.Size(58, 13);
             this.label2.TabIndex = 10;
             this.label2.Text = "Last Name";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(33, 301);
+            this.label3.Location = new System.Drawing.Point(25, 203);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(94, 20);
+            this.label3.Size = new System.Drawing.Size(62, 13);
             this.label3.TabIndex = 11;
             this.label3.Text = "Department";
             // 
-            // btnCancel
+            // openFileD
             // 
-            this.btnCancel.Location = new System.Drawing.Point(196, 420);
-            this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(111, 41);
-            this.btnCancel.TabIndex = 12;
-            this.btnCancel.Text = "Cancel";
-            this.btnCancel.UseVisualStyleBackColor = true;
-            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            this.openFileD.FileName = "openFileDialog1";
             // 
-            // btnAdd
+            // savetext
             // 
-            this.btnAdd.Location = new System.Drawing.Point(37, 420);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(111, 41);
-            this.btnAdd.TabIndex = 13;
-            this.btnAdd.Text = "Add to list";
-            this.btnAdd.UseVisualStyleBackColor = true;
-            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
-            // 
-            // menuStrip2
-            // 
-            this.menuStrip2.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
-            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(24, 24);
-            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.savetext.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.dataSourceToolStripMenuItem});
-            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip2.Name = "menuStrip2";
-            this.menuStrip2.Size = new System.Drawing.Size(1392, 33);
-            this.menuStrip2.TabIndex = 14;
-            this.menuStrip2.Text = "menuStrip2";
+            this.savetext.Location = new System.Drawing.Point(0, 0);
+            this.savetext.Name = "savetext";
+            this.savetext.Size = new System.Drawing.Size(800, 24);
+            this.savetext.TabIndex = 12;
+            this.savetext.Text = "menuStrip1";
             // 
             // dataSourceToolStripMenuItem
             // 
             this.dataSourceToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.openExcel,
+            this.excelFileToolStripMenuItem,
             this.textFileToolStripMenuItem});
             this.dataSourceToolStripMenuItem.Name = "dataSourceToolStripMenuItem";
-            this.dataSourceToolStripMenuItem.Size = new System.Drawing.Size(124, 29);
+            this.dataSourceToolStripMenuItem.Size = new System.Drawing.Size(82, 20);
             this.dataSourceToolStripMenuItem.Text = "Data Source";
             // 
-            // openExcel
+            // excelFileToolStripMenuItem
             // 
-            this.openExcel.Name = "openExcel";
-            this.openExcel.Size = new System.Drawing.Size(270, 34);
-            this.openExcel.Text = "Excel File";
+            this.excelFileToolStripMenuItem.Name = "excelFileToolStripMenuItem";
+            this.excelFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.excelFileToolStripMenuItem.Text = "Excel File";
+            this.excelFileToolStripMenuItem.Click += new System.EventHandler(this.excelFileToolStripMenuItem_Click_1);
             // 
             // textFileToolStripMenuItem
             // 
             this.textFileToolStripMenuItem.Name = "textFileToolStripMenuItem";
-            this.textFileToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.textFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.textFileToolStripMenuItem.Text = "Text File";
-            // 
-            // contextMenuStrip3
-            // 
-            this.contextMenuStrip3.ImageScalingSize = new System.Drawing.Size(24, 24);
-            this.contextMenuStrip3.Name = "contextMenuStrip3";
-            this.contextMenuStrip3.Size = new System.Drawing.Size(61, 4);
-            // 
-            // contextMenuStrip4
-            // 
-            this.contextMenuStrip4.ImageScalingSize = new System.Drawing.Size(24, 24);
-            this.contextMenuStrip4.Name = "contextMenuStrip4";
-            this.contextMenuStrip4.Size = new System.Drawing.Size(61, 4);
+            this.textFileToolStripMenuItem.Click += new System.EventHandler(this.textFileToolStripMenuItem_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1392, 726);
-            this.Controls.Add(this.btnAdd);
-            this.Controls.Add(this.btnCancel);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.dtGridView);
-            this.Controls.Add(this.cbDept);
+            this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.txtLn);
+            this.Controls.Add(this.cbDept);
             this.Controls.Add(this.txtFn);
-            this.Controls.Add(this.menuStrip1);
-            this.Controls.Add(this.menuStrip2);
-            this.MainMenuStrip = this.menuStrip1;
+            this.Controls.Add(this.savetext);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dtGridView)).EndInit();
-            this.menuStrip2.ResumeLayout(false);
-            this.menuStrip2.PerformLayout();
+            this.savetext.ResumeLayout(false);
+            this.savetext.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -251,27 +212,22 @@
 
         #endregion
 
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
         private System.Windows.Forms.TextBox txtFn;
-        private System.Windows.Forms.TextBox txtLn;
         private System.Windows.Forms.ComboBox cbDept;
+        private System.Windows.Forms.TextBox txtLn;
+        private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.DataGridView dtGridView;
+        private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnReset;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private System.Windows.Forms.Button btnCancel;
-        private System.Windows.Forms.Button btnAdd;
-        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.OpenFileDialog openFileD;
+        private System.Windows.Forms.MenuStrip savetext;
         private System.Windows.Forms.ToolStripMenuItem dataSourceToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem openExcel;
+        private System.Windows.Forms.ToolStripMenuItem excelFileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem textFileToolStripMenuItem;
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip3;
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,18 +10,17 @@ using System.Windows.Forms;
 using System.Data.OleDb;
 using System.IO;
 
-namespace PattaguanMj
+namespace _046_Pattaguan_Langcay_L3
 {
     public partial class Form1 : Form
     {
         DataTable dt = new DataTable();
-        DataTable = _dt
-        
+        DataTable _dt = new DataTable();
+
         public Form1()
         {
             InitializeComponent();
         }
-        
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
@@ -37,14 +36,39 @@ namespace PattaguanMj
                 dt.Rows.Add(txtFn.Text, txtLn.Text, cbDept.SelectedItem);
                 dtGridView.DataSource = dt;
             }
-      
+
+
         }
+
         private void Form1_Load(object sender, EventArgs e)
         {
-          
+
+
             dt.Columns.Add("First Name");
             dt.Columns.Add("Last Name");
             dt.Columns.Add("Department");
+        }
+
+        private void excelFileToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            openFileD.Title = "Excel File";
+            openFileD.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            openFileD.Filter = "All Files (.)|*.*|Excel File (.xlsx)|.xlsx";
+            openFileD.FilterIndex = 2;
+            openFileD.ShowDialog();
+
+            OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; " +
+            "Data Source=" + openFileD.FileName + "; Extended Properties='Excel 12.0 Xml; HDR=Yes'");
+            OleDbCommand cmd = new OleDbCommand("SELECT * FROM [Sheet1$]", conn);
+            OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            _dt = dt;
+
+
+            cbDept.DataSource = _dt;
+            cbDept.DisplayMember = "dept";
+
 
         }
 
@@ -63,6 +87,29 @@ namespace PattaguanMj
             txtFn.Text = "";
             txtLn.Text = "";
             cbDept.SelectedIndex = -1;
+        }
+
+        private void openFile_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void textFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveText.Title = "Save as file";
+            saveText.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            saveText.DefaultExt = "txt*;
+            saveText.Filter = "All files (*.*) | *.*| Text File(*.txt) |*.txt";
+            saveText - FilterIndex = 2;
+
+            if (saveText.ShowDialog() = DialogResult.OK)
+            {
+                using (Streashriter writer = File.CreateText(saveText.Filelase))
+                {
+                    writer.WriteLine(txtDisplay.Text);
+                    txtDisplay.Clear();
+                }
+            }
         }
     }
 }
